b1a666178fe136cc9aa9f510f65a2d918d9741a0
Michael Beck <brit228@gmail.com>
2019-04-18 12:05:51 -0400

    Change to datascience flask

---
+++ datascience/application.py
@@ -0,0 +1,22 @@
+#!flask/bin/python
+import json
+import pickle
+from flask import Flask, Response
+from datascience.flaskrun import flaskrun
+
+
+application = Flask(__name__, static_folder=None)
+
+with open("/home/ec2-user/python-flask-service/predict.pickle", "rb") as fp:
+    predict = pickle.load(fp)
+
+@application.route("/predict", methods=["POST"])
+def predict():
+    smiles = request.form.get('smiles')
+    pr_seq = request.form.get('sequence')
+    if smiles != "" and pr_seq != "":
+        return str(predict.predict(data_lig, data_seq))
+    return str(-1.0)
+
+if __name__ == '__main__':
+    flaskrun(application)
