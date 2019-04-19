44598ba7505de46a703f8dc86408996914c13a38
Michael Beck <brit228@gmail.com>
2019-04-18 21:28:23 -0400

    Updated API

--- datascience/application.py
+++ datascience/application.py
@@ -1,21 +1,22 @@
 #!flask/bin/python
 import json
-import pickle
+import dill
+import numpy as np
 from flask import Flask, Response
 from datascience.flaskrun import flaskrun
 
 
 application = Flask(__name__, static_folder=None)
 
-with open("/home/ec2-user/python-flask-service/predict.pickle", "rb") as fp:
-    predict = pickle.load(fp)
+with open("/home/ec2-user/python-flask-service/predict.pk", "rb") as fp:
+    model = pickle.load(fp)
 
 @application.route("/predict", methods=["POST"])
 def predict():
     smiles = request.form.get('smiles')
     pr_seq = request.form.get('sequence')
     if smiles != "" and pr_seq != "":
-        return str(predict.predict(data_lig, data_seq)[0,1])
+        return "{:.5f}".format(model.predict_proba(np.array([[smiles, pr_seq]]))[0,1])
     return str(-1.0)
 
 if __name__ == '__main__':
